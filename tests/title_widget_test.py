b143ace55ae0a2bd319af8fefa84ce3df16ad00b
Jesper Fussing Mørk <jfm@moerks.dk>
2018-10-14 09:25:32 +0200

    Added title widget

---
+++ tests/title_widget_test.py
@@ -0,0 +1,16 @@
+import curses
+from bark.ui.title_widget import TitleWidget
+
+class TestTitleWidget:
+    def setup_method(self, method):
+        curses.initscr()
+        self.title_widget = TitleWidget('title', 1, 1)
+
+    def test_set_title_normal(self):
+        title_text = self.title_widget.set_title('short title')
+        assert title_text == 'short title'
+
+    def test_set_title_long(self):
+        self.title_widget.terminal_width = 10
+        title_text = self.title_widget.set_title('short title')
+        assert title_text == 'short ...'
