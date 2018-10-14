3e22185dcd92927852826dec216a0c486459005b
Jesper Fussing Mørk <jfm@moerks.dk>
2018-10-14 17:44:01 +0200

    Updated screenshot

--- bark/config/consumer.py
+++ bark/config/consumer.py
@@ -1,5 +1,5 @@
 class BarkConsumer:
 
     def __init__(self):
-        self.CONSUMER_KEY=None
-        self.CONSUMER_SECRET=None
+        self.CONSUMER_KEY='QjZrvXKsldQnz47iDYppSSAsz'
+        self.CONSUMER_SECRET='RZV8zkD9AgAZYIsq5mJy5ny574CLrk0nL5oFcb6oJgoFF0ZX52'
